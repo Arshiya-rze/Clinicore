597e6fdac83aee23255a1aa11537351b68ebbf5c
Arshiya <83arshiarezaie83@gmail.com>
2026-02-08 20:57:15 +0330

    Structure of folders have been added to domain layer

---
+++ src/Clinicore.Domain/Identity/Permission.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clinicore.Domain.Identity
+{
+    public class Permission
+    {
+        
+    }
+}
