17f466db3cf26a629f89b7dc4808583af6abb81e
Arshiya <83arshiarezaie83@gmail.com>
2026-02-11 19:25:37 +0330

    Added folders & files in application layer

---
+++ src/Clinicore.Application/Features/Clinic/Sms/SendSms/SendSmsCommand.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clinicore.Application.Features.Clinic.Sms.SendSms
+{
+    public class SendSmsCommand
+    {
+        
+    }
+}
