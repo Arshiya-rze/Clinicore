f34dc0ec3daea98221ee813d1e7e311ccfdd3216
Arshiya <83arshiarezaie83@gmail.com>
2026-02-16 15:59:08 +0330

    Files & folders in webapi layer have been added

---
+++ src/Clinicore.WebApi/Controllers/Platform/PlansController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clinicore.WebApi.Controllers.Platform
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PlansController : ControllerBase
+    {
+        
+    }
+}
