5bcf4918f2d8fe3df85b37a0a09de3fcdab73349
Arshiya <83arshiarezaie83@gmail.com>
2026-02-16 16:59:17 +0330

    Files & folders in contracts layer have been added

---
+++ src/Clinicore.Contracts/Clinic/Treatments/TreatmentResponse.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clinicore.Contracts.Clinic.Treatments
+{
+    public class TreatmentResponse
+    {
+        
+    }
+}
